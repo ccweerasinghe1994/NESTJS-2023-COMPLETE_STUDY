e3433fd3bc5a1a4994e2a9e0150f4500c4faaf2e
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2023-04-04 22:54:29 +0530

    adding mysql database to the application

--- 9 - Custom Data Serialization/src/app.module.ts
+++ 9 - Custom Data Serialization/src/app.module.ts
@@ -4,16 +4,24 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { User } from './users/user.entity';
-import { Report } from './reports/report.entity';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [User, Report],
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: 3306,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [User],
       synchronize: true,
+      ssl: {
+        rejectUnauthorized: true,
+      },
     }),
     UsersModule,
     ReportsModule,
