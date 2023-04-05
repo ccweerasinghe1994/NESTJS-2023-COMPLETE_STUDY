58c67f8424444cdbaad425dcd8329df8ade9ca8c
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2023-04-05 18:37:09 +0530

    Unit Testing a Controller

--- 10 - Getting Started with Unit Testing/src/users/users.controller.aspec.ts
+++ 10 - Getting Started with Unit Testing/src/users/users.controller.aspec.ts
@@ -1,18 +1,35 @@
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { UsersController } from './users.controller';
-//
-// describe('UsersController', () => {
-//   let controller: UsersController;
-//
-//   beforeEach(async () => {
-//     const module: TestingModule = await Test.createTestingModule({
-//       controllers: [UsersController],
-//     }).compile();
-//
-//     controller = module.get<UsersController>(UsersController);
-//   });
-//
-//   it('should be defined', () => {
-//     expect(controller).toBeDefined();
-//   });
-// });
+import { Test, TestingModule } from '@nestjs/testing';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
+import { User } from './user.entity';
+
+describe('UsersController', () => {
+  let controller: UsersController;
+  let FakeUsersService: Partial<UsersService>;
+  let FakeAuthService: Partial<AuthService>;
+
+  beforeEach(async () => {
+    FakeUsersService = {
+      find:()=>{},
+      findOne:()=>{},
+      remove:()=>{},
+      update:()=>{}
+    };
+
+    FakeAuthService = {
+       signIn():()=>{},
+      signUp():()=>{}
+    };
+
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [UsersController],
+    }).compile();
+
+    controller = module.get<UsersController>(UsersController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
--- Docs/readme-10.md
+++ Docs/readme-10.md
@@ -311,6 +311,44 @@ describe('AuthService', () => {
 });
 ```
 ## 105 - Unit Testing a Controller
+```ts
+import { Test, TestingModule } from '@nestjs/testing';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
+import { User } from './user.entity';
+
+describe('UsersController', () => {
+  let controller: UsersController;
+  let FakeUsersService: Partial<UsersService>;
+  let FakeAuthService: Partial<AuthService>;
+
+  beforeEach(async () => {
+    FakeUsersService = {
+      find:()=>{},
+      findOne:()=>{},
+      remove:()=>{},
+      update:()=>{}
+    };
+
+    FakeAuthService = {
+       signIn():()=>{},
+      signUp():()=>{}
+    };
+
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [UsersController],
+    }).compile();
+
+    controller = module.get<UsersController>(UsersController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
+```
+
 ## 106 - More Mock Implementations
 ## 108 - Not Super Effective Tests
 ## 109 - Testing the Signin Method
